e565e44ff20ecd647814ef45b266952d97168b7e
lervendi <lervendi111@gmail.com>
2025-09-01 21:24:51 +0100

    function number_of_the_words was refactored to stats.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 
+from stats import number_of_the_words
+
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
-    
-def number_of_the_words(text):
-    #split text by words
-    words = text.split()
-    #return the number of the words
-    return len(words)
     
 def main():
     book_text = get_book_text("books/frankenstein.txt") 
---
+++ stats.py
@@ -0,0 +1,5 @@
+def number_of_the_words(text):
+    #split text by words
+    words = text.split()
+    #return the number of the words
+    return len(words)
