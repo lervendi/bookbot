695baa43fe45f161e05d27797f182f29979cf764
lervendi <lervendi111@gmail.com>
2025-09-01 21:13:09 +0100

    made function number_of_words which count number of words and remake mainto print number of words

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
     
+def number_of_the_words(text):
+    #split text by words
+    words = text.split()
+    #return the number of the words
+    return len(words)
+    
 def main():
     book_text = get_book_text("books/frankenstein.txt") 
-    print(book_text)
-
+    num_words = number_of_the_words(book_text)
+    print(f"{num_words} words found in the document")
 main()
+
