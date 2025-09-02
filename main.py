ae6d0d5432442b384b491163a0f5f83f09c21811
lervendi <lervendi111@gmail.com>
2025-09-02 14:52:43 +0100

    add word and character count with sys.argv book input

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
-
-from stats import number_of_the_words
+import sys 
+from stats import number_of_the_words, sort_characters
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
     
+def count_character(text):
+    char_dict = {}
+    for char in text:
+        if char.isalpha() and char.isascii():
+            char = char.lower()
+            if char in char_dict:
+                char_dict[char] += 1
+            else:
+                char_dict[char] = 1
+    return char_dict
+
+
 def main():
-    book_text = get_book_text("books/frankenstein.txt") 
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+        
+    book_path = sys.argv[1]
+    book_text = get_book_text(book_path)
+
     num_words = number_of_the_words(book_text)
-    print(f"{num_words} words found in the document")
+    char_list = count_character(book_text)
+    sorted_list = sort_characters(char_list)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+
+    print("DEBUG (first 10 symbols):", sorted_list[:10])
+
+    for item in sorted_list:
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 main()
+
 
--- stats.py
+++ stats.py
@@ -3,3 +3,19 @@ def number_of_the_words(text):
     words = text.split()
     #return the number of the words
     return len(words)
+
+#what is the principle of sorting
+def sort_on(items):
+    return items["num"]
+
+def sort_characters(char_dict):
+    letters = []
+    for char, num in char_dict.items():
+        if char.isalpha():
+            letters.append({"char": char, "num": num})
+    
+    letters.sort(reverse=True, key=sort_on)
+    return letters
+
+
+
